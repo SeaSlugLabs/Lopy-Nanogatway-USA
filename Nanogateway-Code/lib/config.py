453e90fb7629a54b78233e9b4a7f09af6c178cac
SeaSlugLabs <jorgeorta@gmail.com>
2017-06-03 12:57:21 -0500

    Update config.py

--- Nanogateway-Code/lib/config.py
+++ Nanogateway-Code/lib/config.py
@@ -8,8 +8,8 @@ PORT = 1700
 NTP = "pool.ntp.org"
 NTP_PERIOD_S = 3600
 
-WIFI_SSID = 'yourwifissid'
-WIFI_PASS = 'your password'
+WIFI_SSID = 'yourwifissid'#your wifi Name
+WIFI_PASS = 'your password'# Your wifi password
 
 LORA_FREQUENCY = 903900000 #Lora Frequency we are going to use
 LORA_DR = "SF7BW125"   
