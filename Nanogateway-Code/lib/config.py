37d207b803e9653a6a6c435f9e5be96953e43be5
SeaSlugLabs <jorgeorta@gmail.com>
2017-06-03 12:56:42 -0500

    Create config.py

---
+++ Nanogateway-Code/lib/config.py
@@ -0,0 +1,15 @@
+""" LoPy LoRaWAN Nano Gateway configuration options """
+
+GATEWAY_ID = 'xxXXxxXXxxXXxxXX'# Put your TTN Gateway ID here
+
+SERVER = 'router.us.thethings.network'
+PORT = 1700
+
+NTP = "pool.ntp.org"
+NTP_PERIOD_S = 3600
+
+WIFI_SSID = 'yourwifissid'
+WIFI_PASS = 'your password'
+
+LORA_FREQUENCY = 903900000 #Lora Frequency we are going to use
+LORA_DR = "SF7BW125"   
